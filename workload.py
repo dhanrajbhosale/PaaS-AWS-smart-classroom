7d5910a19ab368f277eca4295f33957b1352dd22
dhanrajbhosale <dhanrajbhosale7797@gmail.com>
2023-03-17 01:18:13 -0700

    adding workload generator

---
+++ workload.py
@@ -0,0 +1,65 @@
+from boto3 import client as boto3_client
+import os
+
+input_bucket = "paasawssmartclassroomstack-inputbucket08d572f4-14omuo9vp7mj4"
+output_bucket = "paasawssmartclassroomstack-outputbucket0b6601c4-1se3pnmp0jzqm"
+test_cases = "test_cases/"
+
+
+def clear_input_bucket():
+    global input_bucket
+    s3 = boto3_client('s3')
+    list_obj = s3.list_objects_v2(Bucket=input_bucket)
+    try:
+        for item in list_obj["Contents"]:
+            key = item["Key"]
+            s3.delete_object(Bucket=input_bucket, Key=key)
+    except:
+        print("Nothing to clear in input bucket")
+
+
+def clear_output_bucket():
+    global output_bucket
+    s3 = boto3_client('s3')
+    list_obj = s3.list_objects_v2(Bucket=output_bucket)
+    try:
+        for item in list_obj["Contents"]:
+            key = item["Key"]
+            s3.delete_object(Bucket=output_bucket, Key=key)
+    except:
+        print("Nothing to clear in output bucket")
+
+
+def upload_to_input_bucket_s3(path, name):
+    global input_bucket
+    s3 = boto3_client('s3')
+    s3.upload_file(path + name, input_bucket, name)
+
+
+def upload_files(test_case):
+    global input_bucket
+    global output_bucket
+    global test_cases
+
+    # Directory of test case
+    test_dir = test_cases + test_case + "/"
+
+    # Iterate over each video
+    # Upload to S3 input bucket
+    for filename in os.listdir(test_dir):
+        if filename.endswith(".mp4") or filename.endswith(".MP4"):
+            print("Uploading to input bucket..  name: " + str(filename))
+            upload_to_input_bucket_s3(test_dir, filename)
+
+
+def workload_generator():
+    print("Running Test Case 1")
+    upload_files("test_case_1")
+
+    print("Running Test Case 2")
+    upload_files("test_case_2")
+
+
+clear_input_bucket()
+clear_output_bucket()
+workload_generator()
