658a40833f22fea8090715a5ec816acd549f99e8
dhanrajbhosale <dhanrajbhosale7797@gmail.com>
2023-03-23 01:13:26 -0700

    dynamodb initial loading

--- paa_s_aws_smart_classroom/paa_s_aws_smart_classroom_stack.py
+++ paa_s_aws_smart_classroom/paa_s_aws_smart_classroom_stack.py
@@ -4,10 +4,30 @@ from aws_cdk import (
     aws_s3 as s3,
     aws_lambda as lambda_, Duration
 )
-
+import boto3
+import json
 from configurations import my_constants
 from aws_cdk.aws_lambda_event_sources import S3EventSource
 from constructs import Construct
+
+dynamodb_resource = boto3.resource("dynamodb")
+
+
+def load_dynamodb():
+    dynamodb_table = dynamodb_resource.Table(my_constants.DYNAMODB_TABLE_NAME)
+    f = open('paa_s_aws_smart_classroom/data/student_data.json')
+    data = json.load(f)
+    f.close()
+    with dynamodb_table.batch_writer() as batch:
+        for i in data:
+            batch.put_item(
+                Item={
+                    'id': i['id'],
+                    'name': i['name'],
+                    'major': i['major'],
+                    'year': i['year']
+                }
+            )
 
 
 class PaaSAwsSmartClassroomStack(Stack):
@@ -39,3 +59,4 @@ class PaaSAwsSmartClassroomStack(Stack):
         table.grant_read_data(docker_lambda)
         input_bucket.grant_read(docker_lambda)
         output_bucket.grant_write(docker_lambda)
+        load_dynamodb()
